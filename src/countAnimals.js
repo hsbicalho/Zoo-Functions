29cd7b30f8bbe31086c9c6df0af206abb88434e7
Heitor <hsbicalho@gmail.com>
2022-03-16 15:40:20 -0300

    requisito 5 implementado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    const objSpecie = {};
+    species.forEach((eachSpecie) => { objSpecie[eachSpecie.name] = eachSpecie.residents.length; });
+    return objSpecie;
+  }
+  if (animal[0].sex) {
+    return species.find((findSpecieBySex) => animal[0]
+      .specie === findSpecieBySex.name).residents.filter((specieSex) => specieSex
+      .sex === animal[0].sex).length;
+  }
+  return species.find((findSpecie) => findSpecie.name === animal[0].specie).residents.length;
 }
 
 module.exports = countAnimals;

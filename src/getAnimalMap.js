5e4d616423e7044fe9092e96150b5b47d66335eb
Heitor <hsbicalho@gmail.com>
2022-03-17 18:45:49 -0300

    requisito 8 implementado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function listNames(options, specie) {
+  return specie.residents.reduce((arrayList, animal) => {
+    if (options.sex && animal.sex === options.sex) arrayList.push(animal.name);
+    if (!options.sex) arrayList.push(animal.name);
+    return arrayList;
+  }, []);
 }
 
+function getAnimalMap(options = { includeNames: false, sorted: false, sex: false }) {
+  const animalMap = species.reduce((obj, specie) => {
+    if (options.includeNames) {
+      const animalNamesList = listNames(options, specie);
+      if (options.sorted) {
+        animalNamesList.sort();
+      }
+      obj[specie.location].push({ [specie.name]: animalNamesList });
+    } else {
+      obj[specie.location].push(specie.name);
+    }
+    return obj;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+  return animalMap;
+}
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,53 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const officeHour = 'officeHour';
+const exhibition = 'exhibition';
+
+function getSpecieByWeekDay(day) {
+  const names = [];
+  if (day === 'Monday') return 'The zoo will be closed!';
+  species.forEach((dSpecie) => {
+    if (dSpecie.availability.includes(day)) names.push(dSpecie.name);
+  });
+  return names;
+}
+function getDayByAnimal(animal) {
+  const foundedAnimal = species.find((chosenAnimal) => chosenAnimal.name === animal);
+  return foundedAnimal.availability;
 }
 
+function getFraseByDay(day) {
+  if (day !== 'Monday') return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  return 'CLOSED';
+}
+
+function getDayHour(day) {
+  if (day === 'Monday') {
+    return {
+      Monday: {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!' },
+    };
+  }
+  const objWeekDay = {};
+  objWeekDay[day] = { [officeHour]: getFraseByDay(day), [exhibition]: getSpecieByWeekDay(day) };
+  return objWeekDay;
+}
+function getSchedule(scheduleTarget) {
+  if (species.some((sName) => sName.name === scheduleTarget)) {
+    return getDayByAnimal(scheduleTarget);
+  }
+  if (Object.keys(hours).some((weekDay) => weekDay === scheduleTarget)) {
+    return getDayHour(scheduleTarget);
+  }
+  if (scheduleTarget === undefined || scheduleTarget) {
+    const objWeekDays = Object.keys(hours).reduce((obj, day) => {
+      const finalObj = obj;
+      finalObj[day] = { [officeHour]: getFraseByDay(day), [exhibition]: getSpecieByWeekDay(day) };
+      return finalObj;
+    }, {});
+    return (objWeekDays);
+  }
+}
 module.exports = getSchedule;

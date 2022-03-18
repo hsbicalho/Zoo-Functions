923a2b33df50d9b293824008f39b219ddf6e90d7
Heitor <hsbicalho@gmail.com>
2022-03-17 21:00:35 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employ = employees.find((emp) => emp.id === id);
+  const fisrt = species.find((bixo) => bixo.id === employ.responsibleFor[0]);
+  const arrayAnimals = fisrt.residents.sort((a, b) => b.age - a.age);
+  return [arrayAnimals[0].name, arrayAnimals[0].sex, arrayAnimals[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;

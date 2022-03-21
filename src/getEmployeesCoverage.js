296c3df457ae85a804257014ea1cff5044a69279
Heitor <hsbicalho@gmail.com>
2022-03-20 21:18:23 -0300

    requisito 10 implementado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -21,22 +21,55 @@ function getLocationByAnimal(idEmploy) {
   return arrayLocation;
 }
 
+function getObjEmployees() {
+  const finalObj = employees.reduce((elemento, eachEmploye) => {
+    const objEmoploye = { id: '', fullName: '', species: [], locations: [] };
+    objEmoploye.id = eachEmploye.id;
+    objEmoploye.fullName = `${eachEmploye.firstName} ${eachEmploye.lastName}`;
+    objEmoploye.species = getSpeciesByEmploy(eachEmploye.id);
+    objEmoploye.locations = getLocationByAnimal(eachEmploye.id);
+    elemento.push(objEmoploye);
+    return elemento;
+  }, []);
+  return finalObj;
+}
+
+function getInfoById(id) {
+  const objInfo = { id: '', fullName: '', species: [], locations: [] };
+  objInfo.id = getEmployeeById(id).id;
+  objInfo.fullName = `${getEmployeeById(id).firstName} ${getEmployeeById(id).lastName}`;
+  objInfo.species = getSpeciesByEmploy(id);
+  objInfo.locations = getLocationByAnimal(id);
+  return objInfo;
+}
+
+function getInfoByName(name) {
+  const objInfo = { id: '', fullName: '', species: [], locations: [] };
+  objInfo.id = getEmployeeByName(name).id;
+  objInfo.fullName = `${getEmployeeByName(name).firstName} ${getEmployeeByName(name).lastName}`;
+  const employ = getEmployeeByName(name);
+  objInfo.species = getSpeciesByEmploy(employ.id);
+  objInfo.locations = getLocationByAnimal(employ.id);
+  return objInfo;
+}
+
+function validadeId(info) {
+  if (info.id && employees.some((employe) => info.id === employe.id)) return true;
+  return false;
+}
+
+function validadeName(info) {
+  if (info.name && employees.some((e) => info.name === e.firstName || info.name === e.lastName)) {
+    return true;
+  }
+  return false;
+}
+
 function getEmployeesCoverage(employeInfo) {
-  if (!employeInfo) {
-    const finalObj = employees.reduce((array, eachEmploye) => {
-      const objEmoploye = { id: '', fullname: '', species: [], locations: [] };
-      objEmoploye.id = eachEmploye.id;
-      objEmoploye.fullname = `${eachEmploye.firstName} ${eachEmploye.lastName}`;
-      objEmoploye.species.push(getSpeciesByEmploy(eachEmploye.id));
-      objEmoploye.species.push(getLocationByAnimal(eachEmploye.id));
-      return objEmoploye;
-    }, []);
-    return finalObj;
-  }
-  if (employees.some((employeeId) => employeInfo === employeeId.id
-  || employees.some((employeeName) => employeInfo === employeeName.name))) {
-  }
+  if (!employeInfo) return getObjEmployees();
+  if (validadeId(employeInfo)) return getInfoById(employeInfo.id);
+  if (validadeName(employeInfo)) return getInfoByName(employeInfo.name);
   throw new Error('Informações inválidas');
 }
-getEmployeesCoverage();
+
 module.exports = getEmployeesCoverage;
